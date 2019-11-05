6ff73e79dd7d0383e3162a371020b02d5eefb741
gjyhuang <grace.jy.huang@gmail.com>
2019-11-05 15:34:03 -0500

    added react routes + components for products list and cart

---
+++ client/components/products.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
+import {logout} from '../store';
+
+const Products = () => (
+  <div>
+    <h1>Product list</h1>
+    <hr />
+  </div>
+);
+
+export default Products;
