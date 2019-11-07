79647058563b5f526efa631ce6ab5a95187bb489
Featherweight <vais.emma@gmail.com>
2019-11-06 20:04:08 -0500

    end-of-day commit on Wednesday

--- client/store/cart.js
+++ client/store/cart.js
@@ -1,23 +1,45 @@
+import axios from 'axios';
+import history from '../history';
+
 /* EV: functionalities that should be brought in here:
 1. add an item to the cart
 2. remove an item from the cart
-3. see my cart
+3. see my cart (this is going to be tied to the nav bar - you click a button that says "my cart")
 4. submit my cart (i.e. make purchase) - includes decrementing our stock */
+
+/**
+ * ACTION TYPES
+ */
+const ADDED_TO_CART = 'ADDED_TO_CART';
+const REMOVED_FROM_CART = 'REMOVED_FROM_CART';
+const GOT_CART = 'GOT_CART';
 
 /**
  * INITIAL STATE
  */
-const defaultCart = {};
+const defaultCart = {
+  orderId: null
+};
 
 /**
  * ACTION CREATORS
  */
-// syntax example: const getUser = user => ({type: GET_USER, user});
+const addedToCart = product => ({type: ADD_TO_CART, product});
+const removedFromCart = product => ({type: REMOVE_FROM_CART, product});
+const gotCart = orderId => ({type: GET_CART, orderId});
 
 /**
  * THUNK CREATORS
  */
-// syntax example: export const me = () => async dispatch => {try {} catch (err) {}}
+export const addToCartThunkCreator = product => async dispatch => {
+  try {
+    // ajax to create new row in the orderItem tablee
+    // const productToAdd = await axios.post('/api/product')
+    // dispatch added to cart
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 /**
  * REDUCER
