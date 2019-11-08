fee7f8f44e0aa4f03b928ac99cec3914964997e1
Featherweight <vais.emma@gmail.com>
2019-11-08 11:00:30 -0500

    [feat] shopping cart: connected cart wrapper to redux store

--- client/components/cart.js
+++ client/components/cart.js
@@ -3,11 +3,32 @@ import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
 import {logout} from '../store';
+import {addToCart} from '../store/cart';
+import {removeFromCart} from '../store/cart';
 
-const Cart = () => (
+// if cart empty - load the <cart is empty> component
+export const Cart = () => (
   <div className="shopping-cart">
     <h1>Your shopping cart</h1>
   </div>
 );
 
-export default Cart;
+// get the list of items in the cart from the store
+const mapStateToProps = function(state) {
+  return {
+    itemsInCart: state.cart.products
+  };
+};
+
+const mapDispatchToProps = function(dispatch) {
+  return {
+    // get the add item function from store
+    addItem: dispatch(addToCart(product)),
+    // get the remove item function
+    removeItem: dispatch(removeFromCart(product))
+  };
+};
+
+const ConnectedCart = connect(mapStateToProps, mapDispatchToProps)(Cart);
+
+export default ConnectedCart;
