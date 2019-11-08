dfc073b03dd08a0e54db9a880631d32c779ca8ce
Featherweight <vais.emma@gmail.com>
2019-11-08 11:16:06 -0500

    rendering stateless components in cart wrapper

--- client/components/cart.js
+++ client/components/cart.js
@@ -5,13 +5,27 @@ import {Link} from 'react-router-dom';
 import {logout} from '../store';
 import {addToCart} from '../store/cart';
 import {removeFromCart} from '../store/cart';
+// add import statement for Grace's cart item component and empty cart component!
+// import {CartItems} from './cart/cartItem';
+// import {EmptyCart} from './cart/emptyCart';
 
 // if cart empty - load the <cart is empty> component
-export const Cart = () => (
-  <div className="shopping-cart">
+export const Cart = () =>
+  state.itemsInCart ? (
+    <CartItems
+      cartItems={state.itemsInCart}
+      handleAdd={state.addItem}
+      handleRemove={state.removeItem}
+    />
+  ) : (
+    <EmptyCart />
+  );
+
+{
+  /* <div className="shopping-cart">
     <h1>Your shopping cart</h1>
-  </div>
-);
+  </div> */
+}
 
 // get the list of items in the cart from the store
 const mapStateToProps = function(state) {
