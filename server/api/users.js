3087bf032c4273c4054ca657791369935acb2049
Featherweight <vais.emma@gmail.com>
2019-11-08 10:37:34 -0500

    adjusted users API route

--- server/api/users.js
+++ server/api/users.js
@@ -23,7 +23,7 @@ router.get('/', async (req, res, next) => {
 router.get('/:id', async (req, res, next) => {
   try {
     const user = await User.findByPk(req.params.id, {
-      attributes: ['firstName', 'lastName', 'email']
+      attributes: ['id', 'firstName', 'lastName', 'email']
     });
 
     if (user) {
