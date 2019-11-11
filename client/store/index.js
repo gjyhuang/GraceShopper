e2d94ca731854b4a63e63f008402658ff5505499
gjyhuang <grace.jy.huang@gmail.com>
2019-11-11 15:43:38 -0500

    [feat] cart persists via localStorage

--- client/store/index.js
+++ client/store/index.js
@@ -5,12 +5,21 @@ import {composeWithDevTools} from 'redux-devtools-extension';
 import user from './user';
 import products from './products';
 import cart from './cart';
+import {loadLocalCart, saveLocalCart} from './localStorage';
 
 const reducer = combineReducers({user, products, cart});
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 );
-const store = createStore(reducer, middleware);
+
+const preloadedState = loadLocalCart();
+
+const store = createStore(reducer, preloadedState, middleware);
+store.subscribe(() => {
+  saveLocalCart({
+    cart: store.getState().cart
+  });
+});
 
 export default store;
 export * from './user';
